cb44530fd8d5c95c30ab06471c781fab5545dafa
Peris034 <21it034@charusat.edu.in>
2025-03-07 17:37:34 +0530

    Home modified

--- JPI-frontend/src/components/public/Home.jsx
+++ JPI-frontend/src/components/public/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Banner from './Banner';
-import Products from './Products/Products';
+// import Products from './Products/Products';
 import Certificates from './Certificates/Certificates';
 import Downloads from './Downloads/Downloads';
 import Payments from './Payments/Payments';
-import Inquiry from './Inquiry/Inquiry';
+// import Inquiry from './Inquiry/Inquiry';
 import Contact from './Contact/Contact';
-import WhatsAppForm from './WhatsAppForm';
+// import WhatsAppForm from './WhatsAppForm';
 
 const Home = () => {
     return (
