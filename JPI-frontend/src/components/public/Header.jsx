0fca6b2de26655bfcf95abcb9d958591da228cd0
Peris034 <21it034@charusat.edu.in>
2025-03-18 21:27:05 +0530

    header changed

--- JPI-frontend/src/components/public/Header.jsx
+++ JPI-frontend/src/components/public/Header.jsx
@@ -23,7 +23,7 @@ function Header() {
             {/* Menu - Mobile & Desktop */}
             <div className={`header-menu ${isMenuOpen ? 'open' : ''}`}>
                 <ul className="nav">
-                    <li className="nav-item"><Link to="/" className="nav-link"><i className="fa-solid fa-house"></i> Home...</Link></li>
+                    <li className="nav-item"><Link to="/" className="nav-link"><i className="fa-solid fa-house"></i> Home.....</Link></li>
                     {/* <li className="nav-item"><Link to="/products" className="nav-link"><i className="fa-solid fa-house"></i> Products</Link></li> */}
                     {/* <li className="nav-item"><Link to="/certificates" className="nav-link"><i className="fa-solid fa-certificate"></i> Certificates</Link></li> */}
                     {/* <li className="nav-item"><Link to="/downloads" className="nav-link"><i className="fa-solid fa-download"></i> Downloads</Link></li> */}
